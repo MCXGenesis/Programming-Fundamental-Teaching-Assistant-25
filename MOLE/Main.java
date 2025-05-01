668022aceeebfb9ad0352cb214832ed3f5a6af92
naufalmaula <naufal.maula@gmail.com>
2025-05-01 21:57:00 +0700

    The solution in java

---
+++ MOLE/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(Main.class.getResourceAsStream("mole.txt"));
+        int t = sc.nextInt(); // Number of test cases
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int real = 0;
+            int fake = 0;
+            for (int j = 0; j < n; j++) {
+                String word = sc.next(); 
+                if (word.equals("MOLE")) {
+                    real++;
+                } else {
+                    fake++;
+                }
+                if (real > 2 * fake) {
+                    break;
+                }
+            }
+            if (fake == n) { // All fake moles
+                System.out.println(0);
+            } else { 
+                System.out.println(real + fake);
+            }
+            sc.nextLine();
+        }
+
+    }
+}
